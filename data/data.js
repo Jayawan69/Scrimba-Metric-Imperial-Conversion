538b10e6e9df4b334666650ca352bc0c3ba1bc00
Jayawan Shihong <sumitkkumar3264@gmail.com>
2025-05-22 23:32:32 +0530

    make button interactive

--- data/data.js
+++ data/data.js
@@ -1,14 +1,39 @@
+// 1 meter = 3.281 feet
+// 1 liter = 0.264 gallon
+// 1 kilogram = 2.204 pound
+
+document.querySelector('button').addEventListener('click', ()=>{
+
+})
+
+
+const inputValue = document.querySelector('.input-field').value
+const feetRatio = 3.28084
+const litRatio = 0.2641729
+const kilotRatio = 2.204623
+
+
+
+const conversionValues = {
+  inFeet: inputValue*feetRatio,
+  inMet: inputValue/feetRatio,
+  inGal: inputValue*litRatio,
+  inLit: inputValue/litRatio,
+  inlbs: inputValue*kilotRatio, 
+  inKil: inputValue/kilotRatio 
+}
+
 export const label = [
   {
     title: 'Length (Meter/Feet)',
-    subtitle: '20 meters = 65.616 feet | 20 feet = 6.096 meters'
+    subTitle: `20 meters = ${conversionValues.inFeet.toFixed(3)} feet | 20 feet = ${conversionValues.inMet.toFixed(3)} meters`
   },
   {
     title: 'Volume (Liters/Gallons)',
-    subtitle: '20 liters = 5.284 gallons | 20 gallons = 75.708 liters'
+    subTitle: `20 liters = ${conversionValues.inGal.toFixed(3)} gallons | 20 gallons = ${conversionValues.inLit.toFixed(3)} liters`
   },
   {
     title: 'Mass (Kilograms/Pounds)',
-    subtitle: '20 kilos = 44.092 pounds | 20 pounds = 9.072 kilos'
+    subTitle: `20 kilos = ${conversionValues.inlbs.toFixed(3)} pounds | 20 pounds = ${conversionValues.inKil.toFixed(3)} kilos`
   }
 ]
--- index.js
+++ index.js
@@ -1,23 +1,80 @@
-import {label} from './data/data.js'
+// import {label} from './data/data.js'
 
-const lowerPart = document.getElementById('lower-part')
+document.querySelector('button').addEventListener('click', ()=>{
 
-let innerHTML = ''
 
-label.forEach(info=>{
-  const div = document.createElement('div')
-  div.classList.add('dimensions-section')
+  let inputValue = document.querySelector('.input-field').value
 
-  let title = document.createElement('p')
-  title.textContent = info.title
-  title.classList.add('measurement-title')
+  const feetRatio = 3.28084
+  const litRatio = 0.2641729
+  const kilotRatio = 2.204623
 
-  const subTitle = document.createElement('p')
-  subTitle.textContent = info.title
-  subTitle.classList.add('conversion-ratio')
 
-  div.append(title)
-  div.append(subTitle)
 
-  lowerPart.append(div)
+
+
+
+  const conversionValues = {
+    inFeet: inputValue*feetRatio,
+    inMet: inputValue/feetRatio,
+    inGal: inputValue*litRatio,
+    inLit: inputValue/litRatio,
+    inlbs: inputValue*kilotRatio, 
+    inKil: inputValue/kilotRatio 
+  }
+
+  const label = [
+    {
+      title: 'Length (Meter/Feet)',
+      subTitle: `${inputValue} meters = ${conversionValues.inFeet.toFixed(3)} feet | ${inputValue} feet = ${conversionValues.inMet.toFixed(3)} meters`
+    },
+    {
+      title: 'Volume (Liters/Gallons)',
+      subTitle: `${inputValue} liters = ${conversionValues.inGal.toFixed(3)} gallons | ${inputValue} gallons = ${conversionValues.inLit.toFixed(3)} liters`
+    },
+    {
+      title: 'Mass (Kilograms/Pounds)',
+      subTitle: `${inputValue} kilos = ${conversionValues.inlbs.toFixed(3)} pounds | ${inputValue} pounds = ${conversionValues.inKil.toFixed(3)} kilos`
+    }
+  ]
+
+
+
+
+  const lowerPart = document.getElementById('lower-part')
+
+
+  if(inputValue){
+    render()
+  }
+
+
+
+  function render(){
+    lowerPart.innerHTML = ''
+    label.forEach(info=>{
+      const div = document.createElement('div')
+      div.classList.add('dimensions-section')
+
+      let title = document.createElement('p')
+      title.textContent = info.title
+      title.classList.add('measurement-title')
+
+      const subTitle = document.createElement('p')
+      subTitle.textContent = info.subTitle
+      subTitle.classList.add('conversion-ratio')
+
+      div.append(title)
+      div.append(subTitle)
+
+      lowerPart.append(div)
+    })
+  }
 })
+
+
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
